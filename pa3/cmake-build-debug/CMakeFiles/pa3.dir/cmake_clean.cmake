332e383460052b9785181f218fb22aacee566d81
redish248 <irisha132132@mail.ru>
2020-04-28 10:09:21 +0300

    pa3 work in progress

---
+++ pa3/cmake-build-debug/CMakeFiles/pa3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa3.dir/bank_robbery.c.o"
+  "CMakeFiles/pa3.dir/log.c.o"
+  "CMakeFiles/pa3.dir/main.c.o"
+  "pa3"
+  "pa3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pa3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pa3/pa1.h
@@ -0,0 +1,36 @@
+#ifndef __IFMO_DISTRIBUTED_CLASS_PA1
+#define __IFMO_DISTRIBUTED_CLASS_PA1
+
+#include "banking.h"
+
+typedef enum {
+    FISRT_PHASE = 0,     ///< start
+    SECOND_PHASE,            ///< transfer
+    THIRD_PHASE,             ///< done
+    BALANCE_HISTORY_PHASE   /// balance history
+} Phase;
+
+typedef struct process_t {
+    pid_t pid;
+    local_id id;
+    pid_t parentPid;
+    timestamp_t lamport_time;
+    int writePipes[16];
+    int readPipes[16];
+} process_t;
+
+int readParams(int argc, char *argv[]);
+void openPipes();
+int forkProcesses();
+void closeExtraPipes();
+void sendStart();
+void sendDone();
+void sendStopAll();
+void receiveTransfer(Message* msg);
+void setBalance(timestamp_t time, balance_t balance, balance_t amount);
+void changeBalance(timestamp_t time, balance_t amount);
+void sendBalanceHistory();
+void receiveHistory(Message* msg);
+void prepareHistory();
+
+#endif // __IFMO_DISTRIBUTED_CLASS_PA1
